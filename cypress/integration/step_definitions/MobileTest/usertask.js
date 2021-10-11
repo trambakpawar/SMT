4b6c5c4990319616fa6565dcf0789b1e7a93b3a8
trambak.pawar <trambakpawar19@gmail.com>
2021-10-11 19:01:13 +0530

    master

--- cypress/integration/step_definitions/MobileTest/usertask.js
+++ cypress/integration/step_definitions/MobileTest/usertask.js
@@ -1,19 +1,21 @@
-import { user } from "../../../pageObjects/negative/User/usertask"
+import { user } from "../../pageObjects/User/usertask"
 
-const data = require("../../../../fixtures/negativedata.json")
+const data = require("../../../fixtures/data.json")
 const st = new user()
 Given(/^I login into system$/, function () {
+cy.viewport(1024,1366)
     st.visit()
 });
 
 When(/^I assign the task to system$/, function () {
+
     st.addstatus()
 });
 
 Then(/^Username get displayed in system$/, function () {
     cy.get('body > div.container > div.jumbotron > div:nth-child(2) > div > div').contains(data.name).should("be.visible")
     cy.screenshot()
-}); 
+});
 
 // delete user status
 When(/^I search the user and delete it$/, function () {
